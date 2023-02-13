0f44f30268e8830c2111c6bd10ef2d22742aa91f
Michael Schulz <mi.schulz@hotmail.com>
2023-02-13 21:47:44 +0100

    start index page design

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,6 +2,7 @@ import Head from 'next/head';
 import Image from 'next/image';
 import { Inter } from '@next/font/google';
 import styles from '@/styles/Home.module.css';
+import ButtonRounded from '@/components/atoms/ButtonRounded';
 
 const inter = Inter({ subsets: ['latin'] });
 
@@ -11,9 +12,16 @@ export default function Home() {
       <Head>
         <meta></meta>
       </Head>
-      <div>
-        <h1>How do you want to get to know me?</h1>
-        <div className='dreieck'></div>
+      <div className='h-[100vh] w-[100vw] flex items-center justify-center flex-col relative'>
+        <h1 className='mb-6'>How do you want to get to know me?</h1>
+        <div className='dreieck absolute h-20 w-20  translate-y-10 clip-triangle border-2 bg-black' />
+        <div>
+          <ButtonRounded>Hi</ButtonRounded>
+        </div>
+        <div className='flex gap-8'>
+          <ButtonRounded>Hi</ButtonRounded>
+          <ButtonRounded>Hi</ButtonRounded>
+        </div>
       </div>
     </>
   );
