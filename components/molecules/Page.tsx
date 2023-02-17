70d6dc11fd8e7b74c63209c3233e4a9a2e03f218
Michael Schulz <mi.schulz@hotmail.com>
2023-02-17 21:47:28 +0100

    Add flipbook + animations

--- components/molecules/Page.tsx
+++ components/molecules/Page.tsx
@@ -1,26 +1,46 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 export interface PageProps {
-  frontPageChildren: React.ReactNode;
-  backPageChildren: React.ReactNode;
-  pageZIndex?: number;
+  frontPageChildren?: React.ReactNode;
+  backPageChildren?: React.ReactNode;
+  lastPageChildren?: React.ReactNode;
+  pageZIndex: number;
+  pageNumber: number;
+  currentPage: number;
+  maxLocation: number;
 }
 
 export default function Page({
   frontPageChildren,
   backPageChildren,
   pageZIndex,
+  pageNumber,
+  currentPage,
+  maxLocation,
+  lastPageChildren,
 }: PageProps) {
+  const isFlipped = currentPage > pageNumber;
+
   return (
     <div
-      className={`page border-2 border-red-500 absolute h-full w-full`}
-      style={{ zIndex: pageZIndex }}
+      id={`page-${pageNumber}`}
+      className={`page absolute h-full w-full transition-all duration-500`}
+      style={{ zIndex: isFlipped ? pageNumber : pageZIndex }}
     >
-      <div className='book__page__front absolute w-full h-full top-0 left-0 z-[1] bg-white'>
+      {lastPageChildren}
+      <div
+        className={`${
+          isFlipped ? 'flipped' : ''
+        } page__front transition-all duration-500 origin-left absolute w-full h-full top-0 left-0 z-[1] bg-white `}
+      >
         {frontPageChildren}
       </div>
-      <div className='book__page__back bg-white absolute w-full h-full top-0 left-0 z-0'>
-        {backPageChildren}
+      <div
+        className={`${
+          isFlipped ? 'flipped' : ''
+        } page__back transition-all duration-500 origin-left absolute w-full h-full top-0 left-0 z-0 bg-white border-l-2 border-gray-300`}
+      >
+        <div className='page__back__content'>{backPageChildren}</div>
       </div>
     </div>
   );
--- pages/book-portfolio.tsx
+++ pages/book-portfolio.tsx
@@ -1,12 +1,45 @@
 import Page from '@/components/molecules/Page';
+import { useState } from 'react';
+
+let numberOfPages = 3;
 
 export default function BookPortfolio() {
+  const [currentPage, setCurrentPage] = useState(1);
+  const maxLocation = numberOfPages + 2;
+  currentPage === maxLocation && setCurrentPage(1);
+  const isBookOpen = currentPage > 1;
+  const bookWidth = 300;
+  const gapWidth = bookWidth / 3.75;
+
   return (
     <>
-      <div className='book__container min-h-screen bg-gray-400 flex justify-center items-center'>
-        <div className='relative book  w-[400px] min-h-[70vh] bg-white'>
+      <div
+        className={` book__container min-h-screen bg-gray-200 flex justify-center items-center`}
+        style={{ gap: `${gapWidth}px` }}
+      >
+        <button
+          className={`transition-all duration-300 ${
+            isBookOpen ? 'translate-x-[-100px]' : 'translate-x-0'
+          }`}
+          onClick={() => {
+            if (currentPage > 1) {
+              setCurrentPage(currentPage - 1);
+            }
+          }}
+        >
+          {' '}
+          Back{' '}
+        </button>
+        <div
+          className={`${
+            isBookOpen ? 'translate-x-[50%]' : 'translate-x-0'
+          } relative book w-[300px] transition-all duration-500   fit-content min-h-[70vh] bg-white`}
+        >
           {/* Page 1*/}
           <Page
+            maxLocation={maxLocation}
+            currentPage={currentPage}
+            pageNumber={1}
             frontPageChildren={
               <h1 className='book__page__title'>Front Page 1</h1>
             }
@@ -18,6 +51,9 @@ export default function BookPortfolio() {
 
           {/* Page 2 */}
           <Page
+            maxLocation={maxLocation}
+            currentPage={currentPage}
+            pageNumber={2}
             frontPageChildren={
               <h1 className='book__page__title'>Front Page 2</h1>
             }
@@ -28,6 +64,9 @@ export default function BookPortfolio() {
           />
           {/* Paper 3 */}
           <Page
+            maxLocation={maxLocation}
+            currentPage={currentPage}
+            pageNumber={3}
             frontPageChildren={
               <h1 className='book__page__title'>Front Page 3</h1>
             }
@@ -37,6 +76,19 @@ export default function BookPortfolio() {
             pageZIndex={1}
           />
         </div>
+        <button
+          className={`transition-all duration-300 ${
+            isBookOpen ? 'translate-x-[100px]' : 'translate-x-0'
+          }`}
+          onClick={() => {
+            if (currentPage < maxLocation) {
+              setCurrentPage(currentPage + 1);
+            }
+          }}
+        >
+          {' '}
+          Next{' '}
+        </button>
       </div>
     </>
   );
