62cb9b2275757c762bffcb73ca1e0a9a194dab6e
Michael Schulz <mi.schulz@hotmail.com>
2023-03-08 20:35:20 +0100

    Refac game object class

--- public/game/gameObject.js
+++ public/game/gameObject.js
@@ -4,38 +4,15 @@ export class GameObject {
   constructor(config) {
     this.x = config.x || 0;
     this.y = config.y || 0;
+    this.direction = config.direction || 'down';
     this.sprite = new Sprite({
       gameObject: this,
       src:
         config.src ||
         'game/assets/lab-character-spritesheet-medium-white-eyes.png',
-      useShadow: config.useShadow || false,
+      useShadow: config.useShadow,
     });
-    // this.width = config.width;
-    // this.height = config.height;
-    // this.image = config.image;
-    // this.ctx = config.ctx;
   }
 
-  draw() {
-    this.ctx.drawImage(
-      this.image,
-      // left cut
-      0,
-      // top cut
-      0,
-      // width cut
-      64,
-      // height cut
-      64,
-      // left position
-      this.x,
-      // top position
-      this.y,
-      // width
-      this.width,
-      // height
-      this.height,
-    );
-  }
+  update() {}
 }
